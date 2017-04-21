e044183fa0676c03f08b795509df0165af875564
Daniel Gaiki <daniel.gki@gmail.com>
2017-04-21 13:09:59 +0200

    Start

--- app/src/main/java/com/example/imab/imab_group33/MainActivity.java
+++ app/src/main/java/com/example/imab/imab_group33/MainActivity.java
@@ -3,11 +3,122 @@ package com.example.imab.imab_group33;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //defining view objects
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignup;
+    private TextView textViewSignin;
+    private ProgressDialog progressDialog;
+
+    //defining firebaseauth object
+    private FirebaseAuth firebaseAuth;
+    public static boolean registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+        //if getCurrentUser does not returns null
+        if(firebaseAuth.getCurrentUser() != null){
+            //that means user is already logged in
+            //so close this activity
+            finish();
+
+            //and open profile activity
+            startActivity(new Intent(getApplicationContext(), RecordActivity.class));
+        }
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonSignup = (Button) findViewById(R.id.buttonSignup);
+
+        textViewSignin=(TextView)findViewById(R.id.textViewSignin);
+
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        textViewSignin.setOnClickListener(this);
+        buttonSignup.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+
+        //getting email and password from edit texts
+
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Per favore introduce una email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Per favore introduce una password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Caricando,per favore aspettare ...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            finish();
+                            registration = true;
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        }else{
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Registrazione con Errore",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignup){
+            registerUser();
+        }
+
+        if(view == textViewSignin){
+            //open login activity when user taps on the already registered textview
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/imab/imab_group33/ProfileActivity.java
@@ -0,0 +1,8 @@
+package com.example.imab.imab_group33;
+
+/**
+ * Created by daniel on 4/21/2017.
+ */
+
+public class ProfileActivity {
+}
